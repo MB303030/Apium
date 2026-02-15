03e44f9a8c10de0c86d280a48cc59847c2e2018c
mike <mbrakaz@gmail.com>
2026-02-15 15:04:39 +0200

    adding data file menue items

---
+++ test/data/menuItems.js
@@ -0,0 +1,26 @@
+// test/data/menuItems.js
+/**
+ * List of main menu items in UIKitCatalog
+ * Each item corresponds to a tappable cell in the main table.
+ * If a detail screen has a different header, add a property `expectedHeader`.
+ */
+module.exports = [
+    'Activity Indicators',
+    'Alert Views',
+    'Buttons',
+    'Date Picker',
+    'Image View',
+    'Page Control',
+    'Picker View',
+    'Progress Views',
+    'Search',
+    'Segmented Controls',
+    'Sliders',
+    'Stack Views',
+    'Steppers',
+    'Switches',
+    'Text Fields',
+    'Text View',
+    'Toolbars',
+    'Web View'
+];
